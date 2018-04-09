7f30d0ba6567961240928870535c3dbe81333d7a
Trac Nguyen <nguyendtrac@gmail.com>
2018-04-09 01:58:44 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,15 +1,15 @@
 class Genre
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
     @songs = []
   end
- 
+
   def songs
     @songs
   end
- 
+
   def add_song(song)
     @songs << song
   end
