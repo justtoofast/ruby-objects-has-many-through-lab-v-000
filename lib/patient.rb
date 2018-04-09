971bddf60fe62250273ac39a78afc348e1beb64f
Trac Nguyen <nguyendtrac@gmail.com>
2018-04-09 02:17:23 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,22 @@
+class Patient
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def doctors
+    @appointments.collect do |appt|
+      appt.doctor
+    end
+  end
+end
