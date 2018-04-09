6bb7a1408b59ad6eebb8494a502a3bbe1933aec4
Trac Nguyen <nguyendtrac@gmail.com>
2018-04-09 02:25:49 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -5,13 +5,13 @@ class Patient
     @name = name
     @appointments = []
   end
-
-  def add_appointment(appointment)
-    @appointments << appointment
-  end
-
+  
   def appointments
     @appointments
+  end
+  
+  def add_appointment(appointment)
+    @appointments << appointment
   end
 
   def doctors
